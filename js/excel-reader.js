d0987645dca2b4f50720f1719b0983e9b6879766
Melvin2703 <melvinvanvliet@hotmail.com>
2025-02-28 12:21:19 +0100

    Stand aangepast

--- js/excel-reader.js
+++ js/excel-reader.js
@@ -1,7 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const excelTable = document.getElementById('excel-table');
-
-    // Vervang 'data.xlsx' met de naam van je Excel bestand
     fetch('data/Standing.xlsx')
         .then(response => response.arrayBuffer())
         .then(data => {
@@ -23,15 +20,26 @@ document.addEventListener('DOMContentLoaded', function() {
                 } else {
                     // Data rows
                     tableHTML += '<tr>';
-                    row.forEach(cell => {
-                        tableHTML += `<td>${cell}</td>`;
+                    row.forEach((cell, cellIndex) => {
+                        if (cellIndex === 9) { // Assuming the form column is the 6th column (index 5)
+                            const formBlocks = String(cell).split(' ').map(char => {
+                                let className = '';
+                                if (char === 'W') className = 'w';
+                                if (char === 'G') className = 'g';
+                                if (char === 'V') className = 'v';
+                                return `<span class="form-block ${className}">${char}</span>`;
+                            }).join('');
+                            tableHTML += `<td>${formBlocks}</td>`;
+                        } else {
+                            tableHTML += `<td>${cell}</td>`;
+                        }
                     });
                     tableHTML += '</tr>';
                 }
             });
             
             tableHTML += '</tbody>';
-            excelTable.innerHTML = tableHTML;
+            document.getElementById('excel-table').innerHTML = tableHTML;
         })
         .catch(error => console.error('Error loading Excel file:', error));
 });
